7759cf08264dd5ad68ced8d1240b11c7a4c8c3f8
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-03-16 16:10:05 +0100

    integrate YouTube api into the website

---
+++ lib/fetchLatestVideos.ts
@@ -0,0 +1,35 @@
+import { google } from 'googleapis';
+
+const channelsId: Array<string> = [
+  'UC8butISFwT-Wl7EV0hUK0BQ', // freeCodeCamp
+  'UCvmINlrza7JHB1zkIOuXEbw', // Mike Dane
+  'UCVls1GmFKf6WlTraIb_IaJg', // Distro Tube
+];
+
+export default async () => {
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  if (apiKey === undefined) throw new Error('There is no YOUTUBE_API_KEY in .env.local file')
+  const results = await Promise.all(
+    channelsId.map(async (id) => {
+      const result = await google.youtube('v3').search.list({
+        // @ts-ignore
+        key: apiKey,
+        part: 'snippet',
+        type: 'video',
+        order: 'date',
+        channelId: id,
+        maxResults: 1,
+      });
+      const video = result.data.items[0];
+      return {
+        videoId: video.id.videoId,
+        title: video.snippet.title,
+        description: video.snippet.description,
+        thumbnail: video.snippet.thumbnails.default.url,
+        publishedAt: video.snippet.publishedAt,
+      };
+    })
+  );
+
+  return results;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
-import getParsedMarkdownFile, { getSortedPostsData } from '../lib/loadFromMarkdownFiles';
+import getParsedMarkdownFile, {
+  getSortedPostsData,
+} from '../lib/loadFromMarkdownFiles';
 import ShadedImage from '../components/ui/ShadedImage';
 import Card from '../components/ui/Card';
 import HeroImage from '../components/ui/HeroImage';
+import fetchLatestVideos from '../lib/fetchLatestVideos';
 
 const lorem =
   'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos ratione hic commodi nemo error dolor...';
@@ -30,40 +34,63 @@ const cardsData = [
   },
 ];
 
-export default function Home({ contentHtml, latestPosts }) {
+export default function Home({ contentHtml, latestPosts, latestVideos }) {
   return (
     <>
       <Head>
         <title>Tech Blog</title>
       </Head>
-      <HeroImage imgSrc='/img/hero.jpg'>
-        Hey, Welcome on my website
-      </HeroImage>
+      <HeroImage imgSrc='/img/hero.jpg'>Hey, Welcome on my website</HeroImage>
+      <section></section>
       <section className='px-14 py-4'>
         <article>
           <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
         </article>
         <h2 className='mb-4'>Latest Blog Posts</h2>
-          <section className='grid grid-cols-3 gap-8'>
-          {latestPosts.map(({ title, date, slug, image}) => (
-            <Card key={title} title={title} subtitle={date} imgSrc={image} href={`/blog/${slug}`}>
+        <section className='grid grid-cols-3 gap-8'>
+          {latestPosts.map(({ title, date, slug, image }) => (
+            <Card
+              key={title}
+              title={title}
+              subtitle={date}
+              imgSrc={image}
+              href={`/blog/${slug}`}
+            >
               {lorem}
             </Card>
           ))}
+        </section>
+        <section className='grid grid-cols-3 gap-8'>
+          {latestVideos.map(
+            ({ videoId, title, description, publishedAt, thumbnail }) => (
+              <Card
+                key={title}
+                title={title}
+                subtitle={publishedAt}
+                imgSrc={thumbnail}
+                href={`https://www.youtube.com/watch?v=${videoId}`}
+              >
+                {description}
+              </Card>
+            )
+          )}
         </section>
       </section>
     </>
   );
 }
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const { contentHtml } = await getParsedMarkdownFile('pages/home.md');
-  const {sortedPostsData} = await getSortedPostsData()
+  const { sortedPostsData } = await getSortedPostsData();
+  const latestVideos = await fetchLatestVideos();
   const latestPosts = sortedPostsData.slice(0, 3);
   return {
     props: {
       contentHtml,
       latestPosts,
+      latestVideos,
     },
+    revalidate: 7200,
   };
 }
