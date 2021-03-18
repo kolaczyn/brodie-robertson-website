e0248dc9c27ca02d85ce65e533e40ed2b8e41844
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-03-18 12:21:27 +0100

    wrote test for some components
    finish tests for <Chip />,
    and wrote tests for <PostDescription />

--- components/ui/Chip.test.tsx
+++ components/ui/Chip.test.tsx
@@ -1,8 +1,9 @@
+import { render, fireEvent } from '@testing-library/react';
+
 import Chip from './Chip';
-import { render } from '@testing-library/react';
 
 describe('<Chip />', () => {
-  it('should correctly show label and number', () => {
+  it('should correctly render label and number', () => {
     const { getByText } = render(
       <Chip label='Linux' number={4} onClick={() => {}} />
     );
@@ -11,5 +12,14 @@ describe('<Chip />', () => {
 
     expect(label).toBeTruthy();
     expect(number).toBeTruthy();
+  });
+  it('triggers the function on click', () => {
+    const mockCallback = jest.fn();
+    const { getByRole } = render(
+      <Chip label='Linux' number={4} onClick={mockCallback} />
+    );
+    const btn = getByRole('button');
+    fireEvent.click(btn);
+    expect(mockCallback).toHaveBeenCalled()
   });
 });
---
+++ components/ui/PostDescription.test.tsx
@@ -0,0 +1,32 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import PostDescription from './PostDescription';
+
+describe('<PostDescription />', () => {
+  let expectedProps: {
+    title: string;
+    date: string;
+    category: string;
+    slug: string;
+    children: React.ReactNode;
+  };
+
+  beforeEach(() => {
+    expectedProps = {
+      title: 'How to write tests in Jest',
+      date: '2021-03-18',
+      category: 'React',
+      slug: 'how-to-write-tests-in-jest',
+      children: <p>This is not a tutorial, but a serious question</p>,
+    };
+  });
+
+  it('should correctly render the props on the component', () => {
+    const { getByText, getByRole } = render(<PostDescription {...expectedProps} />);
+    expect(getByText(expectedProps.title)).toBeTruthy();
+    expect(getByText(expectedProps.date)).toBeTruthy();
+    expect(getByText(expectedProps.category)).toBeTruthy();
+    // click the header linking to the blog post
+    expect(getByText(expectedProps.title).closest('a')).toHaveAttribute('href', `/blog/${expectedProps.slug}`)
+  });
+});
