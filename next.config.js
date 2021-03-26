49a50a746a2947757a262a82208a9b72a9b7bcce
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-03-26 15:44:03 +0100

    swap react for preact it should decrease load time I basically copied config from Lee Robinson's website: https://github.com/leerob/leerob.io

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   images: {
-    domains: ['source.unsplash.com', 'i.ytimg.com' ],
+    domains: ['source.unsplash.com', 'i.ytimg.com'],
   },
-}
+  webpack: (config) => {
+    Object.assign(config.resolve.alias, {
+      react: 'preact/compat',
+      'react-dom/test-utils': 'preact/test-utils',
+      'react-dom': 'preact/compat',
+    });
+    return config;
+  },
+};
