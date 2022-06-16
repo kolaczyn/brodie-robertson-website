e7397bcbc1bced808d2ba4ad2bf1e657af5fbc84
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-06-16 18:54:59 +0200

    add storybook with example story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,23 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  stories: ['../stories/**/*.stories.js', '../components/**/*.stories.tsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('tailwindcss'), require('autoprefixer')],
+            },
+          },
+        },
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
 };
